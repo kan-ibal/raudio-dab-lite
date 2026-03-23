f9fa41ef7821f3ea04ae886d324435454508155b
kan-ibal <91786986+kan-ibal@users.noreply.github.com>
2026-03-23 08:15:38 +0100

    escape characters for backslashes

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ mv /usr/bin/dab-rtlsdr-3 /usr/bin/dab-rtlsdr-3.org
 curl -L https://github.com/kan-ibal/raudio-dab-lite/raw/refs/heads/main/dab-rtlsdr-3 -o /usr/bin/dab-rtlsdr-3
 
 
-sed -i 's|-b:a 160k \|-b:a 128k \|'  /srv/http/bash/dab-start.sh
-sed -i 's|-c:a aac \|-c:a mp2 \|'  /srv/http/bash/dab-start.sh
+sed -i 's|-b:a 160k \\|-b:a 128k \\|'  /srv/http/bash/dab-start.sh
+sed -i 's|-c:a aac \\|-c:a mp2 \\|'  /srv/http/bash/dab-start.sh
 
